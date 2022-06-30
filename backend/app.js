f1de5c0b857c85e2e3799e70cf39632bf1a03746
elzaelani <ezaelani72@gmail.com>
2022-06-30 14:09:06 +0700

    tambah file user cintroller,router user,user model utk proses Login

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,20 @@
-import express from 'express'
+import express from 'express' //1
+import dotenv from 'dotenv';
 import db from "./app/config/database.js"
-import productRoutes from './app/routes/index.js'
 import cors from 'cors'
+import productRoutes from './app/routes/Product.js'
+import userRoutes from './app/routes/User.js';
+// import Users from './app/models/UserModel.js'
+dotenv.config();
 
 
-const app= express()
+const app= express() //2
 
 //Connection to Dtabase
 try {
     await db.authenticate();
-    console.log('database Connected')
+    console.log('database Connected');
+    // await Users.sync(); //kode ini brefungsi utk mnggenerate table scara otomatis
 } catch (error) {
     console.log('Connection Error : ',error)
     
@@ -17,7 +22,10 @@ try {
 app.use(cors());
 
 app.use(express.json()) //midlle ware utk menerima body yg dikirim 
+
 // route
+app.use('/login',userRoutes);
+app.use('/user',userRoutes);
 app.use('/products',productRoutes);
 
 app.use('/', (req, res) => {
@@ -27,4 +35,4 @@ app.use('/', (req, res) => {
 
 
 const port =5001
-app.listen(port,()=> console.log(`Servere berjalan di port ${port}`));
+app.listen(port,()=> console.log(`Servere berjalan di port ${port}`)); //3
