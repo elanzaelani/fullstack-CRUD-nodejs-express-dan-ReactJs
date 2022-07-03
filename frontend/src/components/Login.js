7f86c6c0657489a6a6e868fe6405b908efac85d3
elzaelani <ezaelani72@gmail.com>
2022-07-03 13:29:59 +0700

    tambah Register

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+     <section className="hero has-background-grey-light is-fullheight is-fullwidth">
+       <div className="hero-body">
+         <div className="container">
+             <div className="columns is-centered">
+                 <div className="column is-4-desktop">
+                     <form className="box">
+                         <div className="field">
+                             <div className="label">Email or Username</div>
+                         </div>
+                     </form>
+                 </div>
+
+             </div>
+           
+          
+         </div>
+       </div>
+     </section>
+    )
+}
+
+export default Login
